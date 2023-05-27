26c6732b426c154ed087dfe49af70e3faafa6040
Kevin97Palma <67132452+Kevin97Palma@users.noreply.github.com>
2023-05-27 14:09:00 -0500

    Modificacion router y interface

---
+++ Clase/variables.py
@@ -0,0 +1,12 @@
+subtotal = 45 
+iva = 0.12 
+descuento = 0.50 
+total = subtotal + (subtotal*iva) - (subtotal*descuento)
+print(total) 
+nombre = "Kevin Palma"
+espacio= " "
+texto= "Su factura es de:"
+print(nombre+espacio+texto+espacio)
+texto_total= f"Encabezado de factura, {nombre} y estamos aqui para el total es {total}" 
+print(texto_total) 
+3464
---
+++ Clase/variables2.py
@@ -0,0 +1,37 @@
+valor1 = 1056
+valor2 = 10755
+suma = valor1 + valor2 
+print(suma) 
+valor3 = 5
+valor0 = 99
+restA = valor0 - valor3 
+print(restA)
+valorz = 12
+valory = 75
+valors = 12
+valorm = 25
+valorx = 23
+santi = valorm + valorz +valory +valors + valorx
+print (santi)
+valor5 = 12
+valor6 = 11
+valor7 = 13
+mathias = valor5 + valor6 + valor7
+print (mathias)
+a = santi + mathias
+print (a) 
+total3 = santi + a + mathias 
+print(total3)
+# 2 valores que se sumen  y luego 3 valores que igual sumen para que al final los dos totales se resten 
+valord = 65
+valorj = 20
+nemo = valord + valorj 
+print (nemo) 
+valorh =21
+valorb =32
+valork =24
+peko = valorh + valorb + valork 
+print(peko)
+c = peko - nemo
+print (c)
+
